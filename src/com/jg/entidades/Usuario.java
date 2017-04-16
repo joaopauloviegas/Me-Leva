45a717e9c2ead0538f9a4de72063eabfe6335066
Gabriel <Gabriel@Not>
2017-04-16 11:03:51 -0300

    Testando Repositorio BD

--- src/com/jg/entidades/Usuario.java
+++ src/com/jg/entidades/Usuario.java
@@ -1,5 +1,191 @@
 package com.jg.entidades;
 
 public class Usuario {
+	
+	private int id;
+	private String nome;
+	private String email;
+	private String password;
+	private String cpf;
+	private String logradouro;
+	private String complemento;
+	private int numero;
+	private String bairro;
+	private String uf;
+	private int xp;
+	
+	
+	public Usuario(){
+		
+	}
+	
+	
+	public Usuario(int id, String nome, String email, String password,
+			String cpf, String logradouro, String complemento, int numero,
+			String bairro, String uf, int xp) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.password = password;
+		this.cpf = cpf;
+		this.logradouro = logradouro;
+		this.complemento = complemento;
+		this.numero = numero;
+		this.bairro = bairro;
+		this.uf = uf;
+		this.xp = xp;
+	}
+
+
+
+
+
+	public Usuario(String nome, String email, String password, String cpf,
+			String logradouro, String complemento, int numero, String bairro,
+			String uf, int xp) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.password = password;
+		this.cpf = cpf;
+		this.logradouro = logradouro;
+		this.complemento = complemento;
+		this.numero = numero;
+		this.bairro = bairro;
+		this.uf = uf;
+		this.xp = xp;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getCpf() {
+		return cpf;
+	}
+
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+
+	public int getNumero() {
+		return numero;
+	}
+
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+
+	public String getBairro() {
+		return bairro;
+	}
+
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+
+	public String getUf() {
+		return uf;
+	}
+
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+
+	public int getXp() {
+		return xp;
+	}
+
+
+	public void setXp(int xp) {
+		this.xp = xp;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nome=" + nome + ", email=" + email
+				+ ", password=" + password + ", cpf=" + cpf + ", logradouro="
+				+ logradouro + ", complemento=" + complemento + ", numero="
+				+ numero + ", bairro=" + bairro + ", uf=" + uf + ", xp=" + xp
+				+ "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
 
 }
--- src/com/jg/repositorio/RepositorioUsuario.java
+++ src/com/jg/repositorio/RepositorioUsuario.java
@@ -1,5 +1,41 @@
 package com.jg.repositorio;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.jg.entidades.Usuario;
+import com.jg.util.Conexao;
+
 public class RepositorioUsuario {
+	
+	Connection con = Conexao.getConnection();
+	
+	public void cadastrarUsuario(Usuario usuario){
+		
+		String sql = "INSERT INTO usuario(nome,email,password,cpf,logradouro,complemento,numero,bairro,uf,xp)";
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, usuario.getNome());
+			ps.setString(2, usuario.getEmail());
+			ps.setString(3, usuario.getPassword());
+			ps.setString(4, usuario.getCpf());
+			ps.setString(5, usuario.getLogradouro());
+			ps.setString(6, usuario.getComplemento());
+			ps.setInt(7, usuario.getNumero());
+			ps.setString(8, usuario.getBairro());
+			ps.setString(9, usuario.getUf());
+			ps.setInt(10, usuario.getXp());
+			ps.execute();
+			ps.close();
+			
+			System.out.println("Cadastrado com sucesso");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		}
+	
+		
+	}
 
 }
